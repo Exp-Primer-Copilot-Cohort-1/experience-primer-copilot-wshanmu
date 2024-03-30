421ce9621a8b9b44ae36c03f75aea5e031d1443f
Shanmu Wang <73751292+wshanmu@users.noreply.github.com>
2024-03-30 05:31:58 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,7 +1,16 @@
 // create web server
-
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
-var qs = require('querystring');
-
+// create a route for comments
+// create a route for comments/new
+// create a route for comments/:id
+// create a route for comments/:id/edit
+// create a route for comments/:id/delete
+// create a route for comments/:id/update
+// create a route for comments/:id/destroy
+// create a route for comments/:id/remove
+// create a route for comments/:id/remove
+// create a route for comments/:id/destroy
+// create a route for comments/:id/delete
+// create a route for comments/:id/update
+// create a route for comments/:id/edit
+// create a route for comments/:id
+// 
